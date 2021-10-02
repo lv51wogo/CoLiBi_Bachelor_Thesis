d2c05810bbee0cdf96852ef7d0fafe68e21abc19
larav <laravkries900@goolemail.com>
2021-10-02 13:30:36 +0200

    extend search api

--- src/DB/controllers/searchController.js
+++ src/DB/controllers/searchController.js
@@ -6,10 +6,55 @@ const Author = db.author;
 
 exports.searchOccurrence = (req, res) => {
     const searchTerm = req.params.id;
+
     Occurrence.findAll({where:{
             [Op.or]: [
-                {term: {[Op.like]: `%${searchTerm}%`}},
-                {scientificName: {[Op.like]: `%${searchTerm}%`}}
+                {term: {[Op.like]: `${searchTerm}`}},
+                {scientificName: {[Op.like]: `${searchTerm}`}},
+            ]
+        }
+    })
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Error occurred while retrieving occurrences."
+            });
+        });
+}
+
+exports.searchWorks = (req, res) => {
+    const searchTerm = req.params.id;
+
+    Work.findAll({where:{
+            [Op.or]: [
+                {title: {[Op.like]: `${searchTerm}`}},
+                {id: {[Op.like]: `${searchTerm}`}}
+            ]
+        }
+    })
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Error occurred while retrieving works."
+            });
+        });
+}
+
+exports.searchAuthor = (req,res) => {
+    const searchTerm = req.params.id;
+
+    Author.findAll({where:{
+            [Op.or]: [
+                {author: {[Op.like]: `${searchTerm}`}},
+                {id: {[Op.like]: `${searchTerm}`}},
+                {forename: {[Op.like]: `${searchTerm}`}},
+                {surname: {[Op.like]: `${searchTerm}`}}
             ]
         }
     })
@@ -22,4 +67,38 @@ exports.searchOccurrence = (req, res) => {
                     err.message || "Error occurred while retrieving authors."
             });
         });
+}
+
+
+exports.search = (req, res) => {
+    const searchTerm = req.params.id;
+
+    Promise.all([
+        Work.findAll({where:{
+            [Op.or]: [
+                {title: {[Op.like]: `${searchTerm}`}},
+                {id: {[Op.like]: `${searchTerm}`}}
+            ]
+        }
+    }),
+        Occurrence.findAll({where:{
+                [Op.or]: [
+                    {term: {[Op.like]: `${searchTerm}`}},
+                    {scientificName: {[Op.like]: `${searchTerm}`}},
+                ]
+            }
+        }),
+        Author.findAll({where:{
+                [Op.or]: [
+                    {author: {[Op.like]: `${searchTerm}`}},
+                    {id: {[Op.like]: `${searchTerm}`}},
+                    {forename: {[Op.like]: `${searchTerm}`}},
+                    {surname: {[Op.like]: `${searchTerm}`}}
+                ]
+            }
+        })
+        ]
+    ).then(data => {
+        res.send(data)
+    })
 }
