dbc5c3edbf906afda1d420b62d80161c25011f20
larav <laravkries900@goolemail.com>
2021-10-26 10:16:27 +0200

    refactor styling of occurrence.component

--- src/app/src/app/occurrence/occurrence.component.ts
+++ src/app/src/app/occurrence/occurrence.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Occurrence} from "../shared/models/occurrence.model";
+import {DataService} from "../shared/services/data.service";
 
 @Component({
   selector: 'app-occurrence',
@@ -8,10 +9,12 @@ import {Occurrence} from "../shared/models/occurrence.model";
 })
 export class OccurrenceComponent implements OnInit {
   @Input() occurrences?: Occurrence[]
+  searchTerm!: string;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.currentSearchTerm.subscribe(term =>
+    this.searchTerm = term)
   }
-
 }
