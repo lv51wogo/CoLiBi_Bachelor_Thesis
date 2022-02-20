1a6c1636d76dcbf56f822ce4f049992bf313be7a
larav <laravkries900@goolemail.com>
2022-02-20 13:52:17 +0100

    default check one radio option, refactor search component

--- src/app/src/app/search/search.component.ts
+++ src/app/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SearchService} from "./search.service";
 import {Search} from "../shared/models/search.model";
 import {DataService} from "../shared/services/data.service";
@@ -15,46 +15,43 @@ export class SearchComponent implements OnInit {
   work = "work";
   author = "author";
   categoryModel: any;
-  fromValue ='1705';
+  fromValue = '1705';
   toValue = '1996';
 
-  constructor(private searchService: SearchService, private dataService: DataService) { }
+  constructor(private searchService: SearchService, private dataService: DataService) {
+  }
 
   ngOnInit(): void {
   }
 
-  change(val: any){
+  change(val: any) {
     this.categoryModel = val;
   }
 
-  search(term: string):void {
+  search(term: string): void {
 
     this.dataService.changeFrom(this.fromValue)
     this.dataService.changeTo(this.toValue);
+    this.dataService.changeSearchType(this.categoryModel);
+    this.dataService.changeSearchTerm(term);
+    this.dataService.changeOccurrenceFilter([]);
 
     if (this.categoryModel == this.occurrence) {
       this.searchService.searchOccurrences(term).subscribe((data: Search) => {
         this.dataService.changeResult(data);
-        this.dataService.changeSearchType(this.categoryModel);
       })
-      this.dataService.changeSearchTerm(term);
-      this.dataService.changeOccurrenceFilter([]);
     }
-    if (this.categoryModel == this.work){
+
+    if (this.categoryModel == this.work) {
       this.searchService.searchWorks(term).subscribe((data: Search) => {
         this.dataService.changeResult(data);
-        this.dataService.changeSearchType(this.categoryModel);
       })
-      this.dataService.changeSearchTerm(term);
-      this.dataService.changeOccurrenceFilter([]);
     }
-    if (this.categoryModel == this.author){
+
+    if (this.categoryModel == this.author) {
       this.searchService.searchAuthors(term).subscribe((data: Search) => {
         this.dataService.changeResult(data);
-        this.dataService.changeSearchType(this.categoryModel);
       })
-      this.dataService.changeSearchTerm(term);
-      this.dataService.changeOccurrenceFilter([]);
     }
   }
 
