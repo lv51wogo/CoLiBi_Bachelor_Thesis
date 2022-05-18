904ab9444d4cfa6538bea21ab6b50b7f3ec25c16
Franziska Goerg <fg60zira@studserv.uni-leipzig.de>
2022-05-18 17:38:46 +0200

    use json occurences

--- src/DB/seeders/6-20210924092735-occurrence.js
+++ src/DB/seeders/6-20210924092735-occurrence.js
@@ -1,28 +1,22 @@
 'use strict';
-const csv = require('csv-parser')
-const fs = require('fs')
-const path = require('path');
-const occPath = path.join(__dirname, '../Data/TestDataOccurrence.csv');
-const results = [];
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        fs.createReadStream(occPath)
-            .pipe(csv({ separator: ';' }))
-            .on('data',  (data) => results.push(
-              {
-                workId: data.workId,
-                term: data.Term,
-                scientificName: data['Scientific Name'],
-                wordsFrame: data.WordsFrame,
-                sentence: data.Sentence,
+        const Occurrences = require('../Data/TestDatenOccurrences.json')
+        let occurArray = []
+        Occurrences.forEach(item => {
+            console.log(item)
+            occurArray.push({
+                workId: item.workId,
+                term: item.Term,
+                scientificName: item.ScientificName,
+                wordsFrame: item.WordsFrame,
+                sentence: item.Sentence,
                 createdAt: new Date(),
                 updatedAt: new Date()
-            }))
-            .on('end', () => {
-                console.log(results);
-                return queryInterface.bulkInsert('Occurrences', results)
-            });
+            })
+        })
+        return queryInterface.bulkInsert('Occurrences', occurArray)
     },
 
     down: async (queryInterface, Sequelize) => {
